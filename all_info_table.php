4b72b90c3760a10fe084121e865893c2184a10f1
luvumbu <luvumbu.n@gmail.com>
2023-08-25 22:16:26 +0200

    ok pour collect all

---
+++ all_info_table.php
@@ -0,0 +1,74 @@
+	<?php
+	// Liste des element 
+	/*
+	id_get_result_users_nom_2_array_2	
+	id_get_result_users_nom_3_array_2	
+	id_get_result_users_nom_4_array_2	
+	id_get_users_nationality_array_2	
+	id_get_club_nom_complet_array_2	
+
+	id_get_club_departement_array_2	
+	id_get_club_region_array_2	
+	id_get_cat_array_2	
+	id_get_users_naissance_array_2		
+	id_get_result_date_perf_array_2	
+
+	id_get_result_villes_nom_array_2	
+	id_epreuve_sex_array_2	
+	id_get_users_nom_complet_array	
+	id_get_epreuve_nom_complet	
+	get_emplacement	
+	
+	get_rp_array_2	
+	get_vent_array_2	
+	get_result_users_perf_array_2	
+	get_result_users_nom_1_array_2	
+	get_result_users_nom_2_array_2	
+	
+	get_result_users_nom_3_array_2	
+	get_result_users_nom_4_array_2	
+	get_users_nationality_array_2	
+	get_club_nom_complet_array_2	
+	get_club_departement_array_2	
+	
+	get_club_region_array_2	
+	get_cat_array_2	
+	get_users_naissance_array_2	
+	get_result_date_perf_array_2	
+	get_result_villes_nom_array_2	
+	
+	epreuve_sex_array_2	
+	get_users_nom_complet_array	
+	info_all_array_click	
+	info_all_array_ip	
+	info_all_array_src_name	
+	
+	window_location_href	
+	get_epreuve_nom_complet	
+	reg_date		
+	*/
+
+
+
+	// categorie athletisme
+
+	/*
+	https://www.athle.fr/asp.net/main.html/html.aspx?htmlid=25
+
+
+
+	*/
+
+
+	// table de cotation athletisme homme 
+
+
+	/*
+
+	https://www.athle.fr/asp.net/main.html/html.aspx?htmlid=125
+
+	// table de cotation femme 
+
+	https://www.athle.fr/asp.net/main.html/html.aspx?htmlid=122
+	*/
+	?>
